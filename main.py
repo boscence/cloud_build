622f5fff1a87e85a42d189523e3d58deca0bfe55
Andrew Boscence <boscence@gmail.com>
2022-01-01 23:44:01 +0100

    flasked and prep for cloud build

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from src import get_data
 from src import write_data
 
 
-iss_location = get_data.get_iss_location()
-file_staged = write_data.write_locally(iss_location)
-write_data.upload_blob("iss_test", 
-                       f"data/{file_staged}", 
-                       f"iss_5min/{file_staged}")
-print(file_staged)
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def run():
+    iss_location = get_data.get_iss_location()
+    file_staged = write_data.write_locally(iss_location)
+    write_data.upload_blob("iss_test", 
+                        f"data/{file_staged}", 
+                        f"iss_5min/{file_staged}")
+ 
+    return ('', 204)
+
+if __name__ == "__main__":        # on running python app.py
+    app.run(debug=True)                     # run the flask app
